2381d1ac4231a418b93f328b259293e2b88b8827
thisantm <thiago.sant.mach@gmail.com>
2024-08-12 18:21:22 -0300

    refactor: Update main.go to use http.Server for improved server configuration

--- main.go
+++ main.go
@@ -2,15 +2,22 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 )
 
 func main() {
-	port := ":8080"
-	addr := "localhost" + port
+	const port = "8080"
+	addr := "localhost:" + port
 
-	http.Handle("/", http.FileServer(http.Dir(".")))
+	mux := http.NewServeMux()
+	mux.Handle("/", http.FileServer(http.Dir(".")))
+
+	srv := &http.Server{
+		Addr:    ":" + port,
+		Handler: mux,
+	}
 
 	fmt.Printf("Running server at: http://%s\n", addr)
-	http.ListenAndServe(port, nil)
+	log.Fatal(srv.ListenAndServe())
 }
