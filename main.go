33e192cd46bccc77cb4fd25e85f840a0459c9af1
thisantm <thiago.sant.mach@gmail.com>
2024-08-12 20:20:14 -0300

    feat: Add middleware for tracking file server hits

--- main.go
+++ main.go
@@ -6,15 +6,25 @@ import (
 	"net/http"
 )
 
+type apiConfig struct {
+	fileServerHits int
+}
+
 func main() {
 	const port = "8080"
 	addr := "localhost:" + port
+	apiState := apiConfig{
+		fileServerHits: 0,
+	}
 
 	mux := http.NewServeMux()
 
 	fs := http.FileServer(http.Dir("."))
-	mux.Handle("/app/*", http.StripPrefix("/app", fs))
+	fsHandler := http.StripPrefix("/app", fs)
+	mux.Handle("/app/*", apiState.middlewareMetricsInc(fsHandler))
 
+	mux.Handle("/metrics", apiState.middlewareMetricsCount())
+	mux.Handle("/reset", apiState.middlewareMetricsReset())
 	mux.HandleFunc("/healthz", handlerReadiness)
 
 	srv := &http.Server{
@@ -30,4 +40,24 @@ func handlerReadiness(w http.ResponseWriter, req *http.Request) {
 	req.Header.Add("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte(http.StatusText(http.StatusOK)))
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		cfg.fileServerHits++
+		next.ServeHTTP(w, req)
+	})
+}
+
+func (cfg *apiConfig) middlewareMetricsReset() http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		cfg.fileServerHits = 0
+	})
+}
+
+func (cfg *apiConfig) middlewareMetricsCount() http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(fmt.Sprintf("Hits: %d", cfg.fileServerHits)))
+	})
 }
