15b73e9f051f5fb7e55df3bf035a511ccecaa56b
thisantm <thiago.sant.mach@gmail.com>
2024-08-17 22:05:24 -0300

    feat: Implement unit test for CreateChirp

--- database_connection_test.go
+++ database_connection_test.go
@@ -9,8 +9,10 @@ import (
 	"testing"
 )
 
+const testDbPath = "database_test.json"
+
 func TestNewDB(t *testing.T) {
-	defer os.Remove("database_test.json")
+	defer os.Remove(testDbPath)
 
 	data, err := json.MarshalIndent(DBStructure{}, "", "\t")
 	if err != nil {
@@ -25,13 +27,13 @@ func TestNewDB(t *testing.T) {
 		}
 	}{
 		{
-			input: "database_test.json",
+			input: testDbPath,
 			expected: struct {
 				db   *DB
 				data []byte
 			}{
 				db: &DB{
-					path: "database_test.json",
+					path: testDbPath,
 					mux:  &sync.RWMutex{},
 				},
 				data: data,
@@ -40,7 +42,7 @@ func TestNewDB(t *testing.T) {
 	}
 
 	for _, cs := range cases {
-		err := os.Remove("database_test.json")
+		err := os.Remove(testDbPath)
 		if err != nil {
 			log.Print(err)
 		}
@@ -80,6 +82,102 @@ func TestNewDB(t *testing.T) {
 				`The expected data %v is not equal to the actual data %v`,
 				cs.expected.data,
 				data,
+			)
+		}
+	}
+}
+
+func TestCreateChirp(t *testing.T) {
+	defer os.Remove(testDbPath)
+
+	err := os.Remove(testDbPath)
+	if err != nil {
+		log.Print(err)
+	}
+
+	dbTest := DBStructure{
+		Chirps: map[int]chirpValid{
+			0: {
+				Id:   0,
+				Body: "Test 0",
+			},
+			1: {
+				Id:   1,
+				Body: "Test 1",
+			},
+		},
+	}
+
+	data, err := json.MarshalIndent(dbTest, "", "\t")
+	if err != nil {
+		log.Print(err)
+	}
+
+	err = os.WriteFile(testDbPath, data, os.ModePerm)
+	if err != nil {
+		t.Errorf("Failed to create Test Database")
+	}
+
+	cases := []struct {
+		input    string
+		expected DBStructure
+	}{
+		{
+			input: "Hello, World!",
+			expected: DBStructure{
+				Chirps: map[int]chirpValid{
+					0: {
+						Id:   0,
+						Body: "Test 0",
+					},
+					1: {
+						Id:   1,
+						Body: "Test 1",
+					},
+					2: {
+						Id:   2,
+						Body: "Hello, World!",
+					},
+				},
+			},
+		},
+	}
+
+	for _, cs := range cases {
+		db, err := NewDB(testDbPath)
+		if err != nil {
+			t.Errorf("Failed to create Test Database")
+		}
+
+		actual, err := db.CreateChirp(cs.input)
+		if err != nil {
+			t.Errorf("Failed to create Chirp with error: %v", err)
+		}
+
+		if !reflect.DeepEqual(actual, cs.expected.Chirps[2]) {
+			t.Errorf(
+				`The expected chirp %v is not equal to the actual chirp %v`,
+				cs.expected.Chirps[2],
+				actual,
+			)
+		}
+
+		data, err := os.ReadFile(testDbPath)
+		if err != nil {
+			t.Errorf("Failed to read Database")
+		}
+
+		dbData := DBStructure{}
+		err = json.Unmarshal(data, &dbData)
+		if err != nil {
+			t.Errorf("Unsmarshal error %v", err)
+		}
+
+		if !reflect.DeepEqual(dbData, cs.expected) {
+			t.Errorf(
+				`The expected db %v is not equal to the actual db %v`,
+				cs.expected,
+				dbData.Chirps,
 			)
 		}
 	}
