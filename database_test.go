518df7ea43d74804c2ff61ea6f802a807f12e2df
thisantm <thiago.sant.mach@gmail.com>
2024-08-18 21:17:46 -0300

    feat: implemented unit tests for GetChirpsById

--- database_test.go
+++ database_test.go
@@ -11,12 +11,12 @@ import (
 var startState = []DBStructure{
 	{
 		Chirps: map[int]chirpValid{
-			0: {
-				Id:   0,
-				Body: "Test 0",
-			},
 			1: {
 				Id:   1,
+				Body: "Test 0",
+			},
+			2: {
+				Id:   2,
 				Body: "Test 1",
 			},
 		},
@@ -25,7 +25,7 @@ var startState = []DBStructure{
 }
 
 func TestCreateChirp(t *testing.T) {
-	// defer os.Remove(testDbPath)
+	defer os.Remove(testDbPath)
 
 	cases := []struct {
 		input    string
@@ -42,22 +42,22 @@ func TestCreateChirp(t *testing.T) {
 			}{
 				db: DBStructure{
 					Chirps: map[int]chirpValid{
-						0: {
-							Id:   0,
-							Body: "Test 0",
-						},
 						1: {
 							Id:   1,
-							Body: "Test 1",
+							Body: "Test 0",
 						},
 						2: {
 							Id:   2,
+							Body: "Test 1",
+						},
+						3: {
+							Id:   3,
 							Body: "Hello, World!",
 						},
 					},
 				},
 				chirp: chirpValid{
-					Id:   2,
+					Id:   3,
 					Body: "Hello, World!",
 				},
 			},
@@ -70,14 +70,14 @@ func TestCreateChirp(t *testing.T) {
 			}{
 				db: DBStructure{
 					Chirps: map[int]chirpValid{
-						0: {
-							Id:   0,
+						1: {
+							Id:   1,
 							Body: "Hello, World!",
 						},
 					},
 				},
 				chirp: chirpValid{
-					Id:   0,
+					Id:   1,
 					Body: "Hello, World!",
 				},
 			},
@@ -164,8 +164,8 @@ func TestGetChirps(t *testing.T) {
 					startState[0].Chirps,
 				},
 				chirps: []chirpValid{
-					startState[0].Chirps[0],
 					startState[0].Chirps[1],
+					startState[0].Chirps[2],
 				},
 			},
 		},
@@ -223,6 +223,105 @@ func TestGetChirps(t *testing.T) {
 				`The expected chirps %v is not equal to the actual chirps %v`,
 				cs.expected.chirps,
 				actualChirps,
+			)
+		}
+
+		if !reflect.DeepEqual(dbData, cs.expected.dbData) {
+			t.Errorf(
+				`The GetChiprs function must not change the database
+				the expected db %v is not equal to the actual db %v`,
+				cs.expected.dbData,
+				dbData.Chirps,
+			)
+		}
+	}
+}
+
+func TestGetChirpById(t *testing.T) {
+	defer os.Remove(testDbPath)
+
+	cases := []struct {
+		input    int
+		expected struct {
+			dbData DBStructure
+			chirp  chirpValid
+		}
+	}{
+		{
+			input: 1,
+			expected: struct {
+				dbData DBStructure
+				chirp  chirpValid
+			}{
+				dbData: DBStructure{
+					startState[0].Chirps,
+				},
+				chirp: chirpValid{
+					Id:   1,
+					Body: "Test 0",
+				},
+			},
+		},
+		{
+			input: 1,
+			expected: struct {
+				dbData DBStructure
+				chirp  chirpValid
+			}{
+				dbData: DBStructure{},
+				chirp:  chirpValid{},
+			},
+		},
+	}
+
+	if len(cases) != len(startState) {
+		t.Errorf("The number of test cases must be equal to the number of start states")
+	}
+
+	for i, cs := range cases {
+		err := os.Remove(testDbPath)
+		if err != nil {
+			log.Print(err)
+		}
+
+		dbTest := startState[i]
+
+		data, err := json.MarshalIndent(dbTest, "", "\t")
+		if err != nil {
+			log.Print(err)
+		}
+
+		err = os.WriteFile(testDbPath, data, os.ModePerm)
+		if err != nil {
+			t.Errorf("Failed to create Test Database")
+		}
+
+		db, err := NewDB(testDbPath)
+		if err != nil {
+			t.Errorf("Failed to connect to Test Database")
+		}
+
+		dbData := DBStructure{}
+		err = json.Unmarshal(data, &dbData)
+		if err != nil {
+			t.Errorf("Unsmarshal error %v", err)
+		}
+
+		actualChirp, err := db.GetChirpById(cs.input)
+		if err != nil && i == 0 {
+			t.Errorf("Failed to get Chirp with error: %v", err)
+		}
+		if err != nil && i == 1 {
+			if err.Error() != "not found" {
+				t.Errorf("Failed to get Chirp with error: %v", err)
+			}
+		}
+
+		if !reflect.DeepEqual(actualChirp, cs.expected.chirp) {
+			t.Errorf(
+				`The expected chirp %v is not equal to the actual chirp %v`,
+				cs.expected.chirp,
+				actualChirp,
 			)
 		}
 
