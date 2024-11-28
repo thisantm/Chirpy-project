c51042e8a902fcfd655f6fd4604a6add38a5d95e
thisantm <thiago.sant.mach@gmail.com>
2024-11-28 19:31:48 -0300

    refactor: start to migrate to postgres

--- main.go
+++ main.go
@@ -1,24 +1,38 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
+	"github.com/thisantm/Chirpy-project/internal/database"
+
+	_ "github.com/lib/pq"
 )
 
 type apiConfig struct {
 	fileServerHits int
+	dbQueries      *database.Queries
 }
 
 func main() {
+	godotenv.Load()
+	dbURL := os.Getenv("DB_URL")
+	db, err := sql.Open("postgres", dbURL)
+	dbQueries := database.New(db)
+
 	const port = "8080"
 	addr := "localhost:" + port
 	apiState := apiConfig{
 		fileServerHits: 0,
+		dbQueries:      dbQueries,
 	}
 
-	databasePath := "database.json"
-	db, err := NewDB(databasePath)
+	// databasePath := "database.json"
+	// db, err := NewDB(databasePath)
 	if err != nil {
 		log.Fatal("Could not connect to a database")
 	}
@@ -33,9 +47,9 @@ func main() {
 	mux.HandleFunc("GET /admin/metrics", apiState.handlerMetricsCount)
 	mux.HandleFunc("GET /api/reset", apiState.handlerMetricsReset)
 	mux.HandleFunc("GET /api/healthz", handlerReadiness)
-	go mux.HandleFunc("POST /api/chirps", apiState.handlerCreateChirp(db))
-	go mux.HandleFunc("GET /api/chirps/", apiState.handlerGetChirps(db))
-	go mux.HandleFunc("GET /api/chirps/{id}", apiState.handlerGetChirpById(db))
+	// go mux.HandleFunc("POST /api/chirps", apiState.handlerCreateChirp(db))
+	// go mux.HandleFunc("GET /api/chirps/", apiState.handlerGetChirps(db))
+	// go mux.HandleFunc("GET /api/chirps/{id}", apiState.handlerGetChirpById(db))
 
 	srv := &http.Server{
 		Addr:    ":" + port,
