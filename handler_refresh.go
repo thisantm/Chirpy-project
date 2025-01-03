3e8c4a15d5720963ed4f5688c86b9f933a45bd47
thisantm <thiago.sant.mach@gmail.com>
2025-01-03 11:26:53 -0300

    refactor: move handlerRevoke to handler_refresh

--- handler_refresh.go
+++ handler_refresh.go
@@ -34,3 +34,18 @@ func (cfg *apiConfig) handlerRefresh(w http.ResponseWriter, req *http.Request) {
 		Token: accessToken,
 	})
 }
+
+func (cfg *apiConfig) handlerRevoke(w http.ResponseWriter, req *http.Request) {
+	refreshToken, err := auth.GetBearerToken(req.Header)
+	if err != nil {
+		respondWithError(w, http.StatusUnauthorized, "authorization header missing", err)
+		return
+	}
+
+	err = cfg.db.RevokeRefreshToken(req.Context(), refreshToken)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "failed to revoke", err)
+	}
+
+	respondWithJSON(w, http.StatusNoContent, "")
+}
