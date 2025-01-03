c355ddf6c64ad5220c914cc03c8e152cff259858
thisantm <thiago.sant.mach@gmail.com>
2025-01-03 14:07:54 -0300

    refactor: remove unused respondWithNoBody function from response.go

--- response.go
+++ response.go
@@ -34,7 +34,3 @@ func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
 	w.WriteHeader(code)
 	w.Write(dat)
 }
-
-func respondWithNoBody(w http.ResponseWriter, code int) {
-	w.WriteHeader(code)
-}
